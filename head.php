569fd2abfe0f55a9aba6635d1ed57d5fee8a9413
Sri Rakesh <sri.rakesh@runo.in>
2025-06-02 18:04:36 +0530

    changes speed

--- head.php
+++ head.php
@@ -1,75 +1,53 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+<!-- head.php -->
 
-<!-- SEO Meta -->
-<meta name="description" content="Runo Call Management App to manage calls at SIM level. Get Call Tracking for outbound sales calls with in-built CRM. Try the best telecaller app for Free.">
-<meta name="keywords" content="Call Management CRM, Telecalling CRM, Outbound CRM, Call Center CRM, Auto Dialer, Telecalling App, Dialer App">
-<meta name="robots" content="no-index, follow">
-<meta name="language" content="English">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
 
-<!-- Open Graph / Facebook -->
-<meta property="og:type" content="website">
-<meta property="og:title" content="Runo - Best Outbound Call Center CRM with Auto Dialer">
-<meta property="og:description" content="Runo Call Management App with Call Tracking, Telecalling CRM, and Auto Dialer.">
-<meta property="og:image" content="img/share-icon.png">
-<meta property="og:url" content="img/share-icon.png">
+	<!-- SEO Meta -->
+	<meta name="description" content="Runo Call Management App to manage calls at SIM level. Get Call Tracking for outbound sales calls with in-built CRM. Try the best telecaller app for Free.">
+	<meta name="keywords" content="Call Management CRM, Telecalling CRM, Outbound CRM, Call Center CRM, Auto Dialer, Telecalling App, Dialer App">
+	<meta name="robots" content="noindex, follow">
+	<meta name="language" content="English">
 
-<!-- Twitter Card -->
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="Runo - Best CRM for Telecalling">
-<meta name="twitter:description" content="Runo CRM helps manage outbound sales calls with in-built CRM & Dialer.">
-<meta name="twitter:image" content="img/share-icon.png">
+	<!-- Open Graph -->
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Runo - Best Outbound Call Center CRM with Auto Dialer">
+	<meta property="og:description" content="Runo Call Management App with Call Tracking, Telecalling CRM, and Auto Dialer.">
+	<meta property="og:image" content="img/share-icon.png">
+	<meta property="og:url" content="https://www.runo.in/">
 
-<!-- Favicon -->
-<link rel="icon" type="image/x-icon" href="img/favicon.ico" sizes="16x16">
+	<!-- Twitter Card -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:title" content="Runo - Best CRM for Telecalling">
+	<meta name="twitter:description" content="Runo CRM helps manage outbound sales calls with in-built CRM & Dialer.">
+	<meta name="twitter:image" content="img/share-icon.png">
 
+	<!-- Favicon -->
+	<link rel="icon" type="image/x-icon" href="img/favicon.ico" sizes="16x16">
 
-<link rel="preload" as="style" href="css/bootstrap.min.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<!-- Preconnect for Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-<!-- Google Fonts -->
-<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+	<!-- Google Fonts -->
+	<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" onload="this.onload=null;this.rel='stylesheet'">
+	<noscript>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap">
+	</noscript>
 
-<!-- Stylesheets -->
-<link rel="stylesheet" href="css/bootstrap.min.css" media="screen">
-<link rel="stylesheet" href="css/slicknav.min.css">
-<link rel="stylesheet" href="css/swiper-bundle.min.css">
-<link rel="stylesheet" href="css/all.min.css">
-<link rel="stylesheet" href="css/animate.css">
-<link rel="stylesheet" href="css/magnific-popup.css">
-<link rel="stylesheet" href="css/custom.css">
-<link rel="stylesheet" href="css/runo.css">
+	<!-- Critical CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/custom.css">
+	<link rel="stylesheet" href="css/runo.css">
 
-<!-- Animate.css and WOW.js -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" defer></script>
-<script>
-	document.addEventListener("DOMContentLoaded", () => {
-		new WOW().init();
-	});
-</script>
+	<!-- Lazy CSS -->
+	<link rel="stylesheet" href="css/slicknav.min.css" media="print" onload="this.media='all'">
+	<link rel="stylesheet" href="css/swiper-bundle.min.css" media="print" onload="this.media='all'">
+	<link rel="stylesheet" href="css/animate.css" media="print" onload="this.media='all'">
+	<link rel="stylesheet" href="css/magnific-popup.css" media="print" onload="this.media='all'">
 
-<!-- Font Awesome (CDN) -->
-<link
-	rel="stylesheet"
-	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-	integrity="sha512-vALH+lMIG+YiQtxv+1BNXz7fhcFt5fhFfQPBKJxK7t0OVP9B16czGmRWef2a9tz+uVplN0u6Qe5Rh6GHmZsEVQ=="
-	crossorigin="anonymous"
-	referrerpolicy="no-referrer" />
-
-<!-- Swiper CSS (from CDN) -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css">
-
-<!-- Swiper JS (CDN, defer to reduce INP impact) -->
-<script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
-
-<!-- UTM Tracker -->
-<script>
-	const params = new URLSearchParams(window.location.search);
-	const utmSource = params.get("utm_source");
-	const utmCampaign = params.get("utm_campaign");
-	if (utmSource) localStorage.setItem("utm_source", utmSource);
-	if (utmCampaign) localStorage.setItem("utm_campaign", utmCampaign);
-</script>
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" crossorigin="anonymous" referrerpolicy="no-referrer">
+</head>
